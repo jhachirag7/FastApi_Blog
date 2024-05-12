4fcffd8ff95bb0e9d0912b946d5ab9e616c605b2
jhachirag7 <jhachirag7@gmail.com>
2024-05-12 19:32:25 +0530

    jwt changes

--- auth/jwttoken.py
+++ auth/jwttoken.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from database.models import TokenData
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALGORITHM = os.environ.get('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES')
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
